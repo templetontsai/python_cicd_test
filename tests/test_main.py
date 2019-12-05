d65c1f87feee807eaf22a5825a8557635c6752e0
Templeton Tsai <tsai.templeton@gmail.com>
2019-12-05 12:55:31 +1100

    Add more functionalities and tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,10 +2,10 @@
 
 import pytest
 
-from main import multiply
+from main import multiply, devide
 
 
-class TestClass():
+class TestClass:
     @pytest.mark.parametrize("num", [(3, 4),
                                      (3.0, 4.0)])
     def test_multiply(self, num):
@@ -13,3 +13,11 @@ class TestClass():
             pytest.raises(ValueError)
         else:
             multiply(num[0], num[1])
+
+    @pytest.mark.parametrize("num", [(3, 4),
+                                     (3.0, 4.0)])
+    def test_devide(self, num):
+        if not isinstance(num[0], int) or not isinstance(num[1], int):
+            pytest.raises(ValueError)
+        else:
+            devide(num[0], num[1])
